190a68ba38a52afbe6be5e55a69f22f44636b526
arielbk <buchwald.kearney@gmail.com>
2019-08-12 22:14:45 +0200

    Remove projects file source

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,13 +49,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `projects`,
-        path: `${__dirname}/src/projects`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
